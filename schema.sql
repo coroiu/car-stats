90f8539efd3eb1ecbf2ebcb802217f8887ecdb48
Andreas Coroiu <andreas.coroiu@gmail.com>
2023-03-27 20:29:28 +0200

    Add data into postgres instead

---
+++ schema.sql
@@ -0,0 +1,28 @@
+-- Adminer 4.8.1 PostgreSQL 15.2 (Debian 15.2-1.pgdg110+1) dump
+
+DROP TABLE IF EXISTS "car";
+CREATE TABLE "public"."car" (
+    "id" character varying NOT NULL,
+    "fullName" text NOT NULL,
+    "title" text NOT NULL,
+    "brand" character varying NOT NULL,
+    "model" character varying NOT NULL,
+    "year" integer NOT NULL,
+    "mileage" integer NOT NULL,
+    "condition" character varying NOT NULL,
+    "fuel" character varying NOT NULL,
+    "locationName" character varying(250) NOT NULL,
+    "facilityName" character varying(250) NOT NULL,
+    CONSTRAINT "car_id" PRIMARY KEY ("id")
+) WITH (oids = false);
+
+
+DROP TABLE IF EXISTS "price";
+CREATE TABLE "public"."price" (
+    "carId" character varying(100) NOT NULL,
+    "price" integer NOT NULL,
+    "createdAt" timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL
+) WITH (oids = false);
+
+
+-- 2023-03-27 18:29:04.02296+00
--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,18 @@
+import { Client } from 'pg';
 import { HedinScraper } from './scrapers/hedin/hedin.scraper';
-import { InfluxDB, Point } from '@influxdata/influxdb-client';
 import { config } from './config';
 import { CarModel } from './models/car.model';
 
 const limit = 500000;
+// const limit = 20;
 const printInterval = 500;
 const unknown = 'Unknown';
-
-function convertToPoint(car: CarModel): Point {
-  return new Point('car-scrape')
-      .stringField('id', car.id)
-      .stringField('fullName', car.fullName)
-      .stringField('title', car.title ?? unknown)
-      .uintField('year', car.year)
-      .uintField('mileage', car.mileage)
-      .intField('price', car.price)
-      .tag('brand', car.brand ?? unknown)
-      .tag('model', car.model ?? unknown)
-      .tag('condition', car.condition)
-      .tag('fuel', car.fuel)
-      .tag('locationName', car.locationName)
-      .tag('facilityName', car.facilityName ?? unknown);
-}
 
 async function run() {
   console.log("Initializing...");
   const hedinScraper = new HedinScraper();
-  const client = new InfluxDB({url: config.influxDb.url, token: config.influxDb.token});
-  const writeApi = client.getWriteApi(config.influxDb.org, config.influxDb.bucket);
-  writeApi.useDefaultTags({ agent: 'car-stats-v1.0' });
+  const client = new Client({ host: config.postgresql.host, port: config.postgresql.port, user: config.postgresql.username, password: config.postgresql.password, database: config.postgresql.database });
+  await client.connect();
 
   let i = 0;
   for await (const car of hedinScraper.scrape()) {
@@ -36,8 +20,7 @@ async function run() {
       console.log(`Parsing Hedin, processed entries: ${i}`);
     }
 
-    const point = convertToPoint(car).tag('source', 'hedin');
-    writeApi.writePoint(point)
+    await insertIntoSQL(client, car);
 
     if (++i > limit) {
       break;
@@ -46,7 +29,31 @@ async function run() {
 
   console.log(`Finished! Total entries processed: ${i}`);
 
-  await writeApi.close();
+  await client.end();
+}
+
+async function insertIntoSQL(client: Client, car: CarModel): Promise<void> {
+  await client.query(
+    `INSERT INTO car(
+        "id",
+        "fullName",
+        "title",
+        "brand",
+        "model",
+        "year",
+        "mileage",
+        "condition",
+        "fuel",
+        "locationName",
+        "facilityName"
+      ) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) ON CONFLICT DO NOTHING`,
+      [car.id, car.fullName, car.title ?? unknown, car.brand ?? unknown, car.model ?? unknown, car.year, car.mileage, car.condition,
+      car.fuel, car.locationName ?? unknown, car.facilityName ?? unknown]);
+  
+  await client.query(
+    `INSERT INTO price("carId", "price") VALUES($1, $2)`,
+    [car.id, car.price]
+  )
 }
 
 run().catch(console.error);
